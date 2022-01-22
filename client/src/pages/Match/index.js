47c94e77a9b00cdbe0b23d8b4fbcdf35df2a0c67
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2022-01-22 16:29:18 -0500

    chat html

--- client/src/pages/Match/index.js
+++ client/src/pages/Match/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 // user/match are objects of {name, ingredients list, ID}
@@ -6,10 +6,13 @@ import './style.css';
 // chat is list of message objects – {name, message}
 // onChat is callback function for when this client sends new chat
 export default function Match({ user, match, recipies, chat, onChat }) {
+
     console.log(user, match);
+
+    const [messageInput, setMessageInput] = useState('');
+
     const commonIngredients = match ? user.ingredients
         .filter(ingredient => match.ingredients.includes(ingredient)) : [];
-    const hasRecipes = (recipies.length != 0)
 
     return !match ? <h3>loading...</h3> :
             <>
@@ -17,40 +20,39 @@ export default function Match({ user, match, recipies, chat, onChat }) {
             <p>Your and match have the following ingredients in commmon:</p>
             <p>{commonIngredients.join(', ')}</p>
 
-            //recipe suggestion
             <div className="horizontal-scrolling-wrapper">
-                    {hasRecipes ? (
-                            <h2> No recipe suggestion, go crazy with your match! </h2>
-                      ) : (
-                                <h2>Suggested Recipes</h2>
-                                {recipies.map(recipie =>
-                                    <div className="recipe-card">
-                                        <h3>{recipie["name"]}</h3>
-                                        <h3>{recipie["ingredients"]}</h3>
-                                        <h3>{recipie["instructions"]}</h3>
-                                    </div>)
-                                }
-                            )
-                       }
+                {!recipies ? <h2> No recipe suggestion, go crazy with your match! </h2> : (
+                        <>
+                            <h2>Suggested Recipes</h2>
+                            {recipies.map(recipie =>
+                                <div className="recipe-card">
+                                    <h3>{recipie["name"]}</h3>
+                                    <h3>{recipie["ingredients"]}</h3>
+                                    <h3>{recipie["instructions"]}</h3>
+                                </div>)
+                            }
+                        </>
+                )}
             </div>
 
-            //chat display box
             <div className="vertical-scrolling-wrapper">
                 <h2>Say Hi and Start Cooking!</h2>
-                {chat["messages"].map(message =>
-                    <p>Sender: {message["sender"]}</p>
-                    <p>Message: {message["text"]}</p>
-                }
+                {chat.map(message =>
+                    <>
+                        <p>Sender: {message["sender"]}</p>
+                        <p>Message: {message["text"]}</p>
+                    </>
+                )}
             </div>
 
-            //chat enter text box
             <div className="message-box">
-              <input type="text" placeholder="enter your message..." <br><br>
-              <input type="submit" value="SEND" onChat DO????>
+                <input type="text" placeholder="enter your message..."
+                    value={messageInput}
+                    onChange={evt => setMessageInput(evt.target.value)}
+                />
+                <br /><br />
+                <button onClick={() => onChat(messageInput)}>Send</button>
             </div>
-
-
-
         </>
     
 }
