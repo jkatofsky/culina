4613bee0b0858923c22d76530c9b805421fea647
David Peterman <peterman.dd@gmail.com>
2022-01-22 13:50:10 -0500

    Mostly finished implementing front-end of landing page. Also added skeleton for /match.

--- client/src/pages/Match/index.js
+++ client/src/pages/Match/index.js
@@ -1,10 +1,54 @@
 import React from 'react';
 
-// user/match are objects of {name, ingredients list, ID}
+// user/partner are objects of {name, ingredients list, ID}
 // recipies is list of recipie objects (format not finalized)
 // chat is list of message objects (format not finalized)
 // onChat is callback function for when this client sends new chat
-export default function Match({ user, match, recipies, chat, onChat }) {
-    // TODO: if !match, render loading state
-    return <h1>match</h1>;
+export default function Match({user, partner, recipies, chat, onChat}) {
+
+    return <>
+    
+    {partner ? <div className="wrapper">
+        <header>
+            <h1> Your match: </h1>
+            <img alt="Match" title="Match!" width="200" height="200"></img> 
+            <ul>
+            <li>Name: </li>
+            <li>Role: </li>
+            <li>Pronouns: </li>
+            <li>Ingredients list: </li>
+            </ul>
+            </header>
+            <hr></hr>
+        <section id="two_column" className="clearfix">
+            <h2> Your recipes: </h2>
+
+
+                <div>
+            <div>RECIPE 1</div>
+            <img alt="Best Recipe" title="Recipe" width="100%"></img>
+            </div>
+
+            <div>
+            <div>RECIPE 2</div>
+            <div className="img1"><img alt="Second best recipe" title="Recipe" width="100%"></img></div>
+            <br></br>
+                <div>RECIPE 3</div>
+            <div className="img2"><img alt="Third best recipe" title="Recipe" width="100%"></img></div>
+            </div>
+
+        </section>
+
+
+
+        <footer>
+            &copy; Culina
+        </footer>
+        </div>
+    : 
+    <div></div>
+    }
+    
+    
+    </>;
 }
