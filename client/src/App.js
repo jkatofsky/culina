5f012ed07463cd3aeae0ef811f9d21c7e91e0ebb
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2022-01-22 00:30:46 -0500

    more frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
+import Landing from './pages/Landing';
+import Search from './pages/Search';
+import Match from './pages/Match';
+
 import './App.css';
 
 function App() {
-  return <></>;
+  return <Router>
+      <Switch>
+    {/* click button and create user with id before redirecting */}
+      <Route exact path="/">
+          <Landing />
+      </Route>
+      {/* when user loads this page, event emitted that they are searching */}
+      <Route path="/search/:id">
+          <Search />
+      </Route>
+      {/* renders each id if they are matched with the other */}
+      <Route path="/match/:id1/:id2">
+          <Match />
+      </Route>
+  </Switch>
+</Router>;
 }
 
 export default App;
---
+++ client/src/pages/Match/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Match() {
+    return <h1>match</h1>;
+}
