d46eed43e7bdc55155058ee6e93cbc759ef3ad51
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2022-01-22 19:56:33 -0500

    layout the match page

--- client/src/pages/Match/index.js
+++ client/src/pages/Match/index.js
@@ -9,43 +9,50 @@ export default function Match({ user, match, recipies, chat, onSendMessage }) {
         .filter(ingredient => match.ingredients.includes(ingredient)) : [];
 
     return !match ? <h3>loading...</h3> :
-            <>
-            <h1 > Your Match is { match.name } </h1 >
-            <p>Your and match have the following ingredients in commmon:</p>
-            <p>{commonIngredients.join(', ')}</p>
+        <>
+            <div className="heading">
+                <h1> your match is {match.name}!</h1 >
+                <small>You have the following ingredients in commmon:</small>
+                <h3>{commonIngredients.join(', ')}</h3>
+            </div>
 
-            <div className="horizontal-scrolling-wrapper">
-                {!recipies ? <h2> No recipe suggestion, go crazy with your match! </h2> : (
-                        <>
+            <div className='content'>
+                <div className="recipies">
+                    {!recipies ?
+                        <p> We were unable to find a recipie suggestion based on your mutual ingredients; we still encourage you to use the chat (and your imagination) to plan something delicious!</p> : <>
+                        {/* TODO: turn the recipies into accordions */}
+                        {/* TODO: turn the ingredients and instructions into bulleted lists – should be easy because they are lists alread I think */}
                             <h2>Suggested Recipes</h2>
                             {recipies.map(recipie =>
                                 <div className="recipe-card">
-                                    <h3>{recipie["name"]}</h3>
-                                    <h3>{recipie["ingredients"]}</h3>
-                                    <h3>{recipie["instructions"]}</h3>
+                                    <h3>{recipie.name}</h3>
+                                    <h3>{recipie.ingredients}</h3>
+                                    <h3>{recipie.instructions}</h3>
                                 </div>)
                             }
                         </>
-                )}
-            </div>
+                    }
+                </div>
 
-            <div className="vertical-scrolling-wrapper">
-                <h2>Say Hi and Start Cooking!</h2>
-                {chat.map(message =>
-                    <>
-                        <p>Sender: {message["sender"]}</p>
-                        <p>Message: {message["message"]}</p>
-                    </>
-                )}
-            </div>
-
-            <div className="message-box">
-                <input type="text" placeholder="enter your message..."
-                    value={messageInput}
-                    onChange={evt => setMessageInput(evt.target.value)}
-                />
-                <br /><br />
-                <button onClick={() => onSendMessage(messageInput)}>Send</button>
+                <div className="chat">
+                    <div className="vertical-scrolling-wrapper">
+                        <h2>Say hi and plan your cooking!</h2>
+                        {chat.map(message =>
+                                <p><b>{message.sender}</b> {message.message}</p>
+                        )}
+                    </div>
+                    <div>
+                        <input type="text" placeholder="enter your message..."
+                            value={messageInput}
+                            onChange={evt => setMessageInput(evt.target.value)}
+                        />
+                        <br /><br />
+                        <button onClick={() => {
+                            onSendMessage(messageInput);
+                            setMessageInput('');
+                        }}>Send</button>
+                    </div>
+                </div>
             </div>
         </>
     
